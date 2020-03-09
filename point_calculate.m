eaafa85ee411a8834656b57d9098e5096025027e
Anastasia <nastyhram@mail.ru>
2020-03-09 16:34:36 +1000

    add files

--- point_calculate.m
+++ point_calculate.m
@@ -1,5 +1,5 @@
 function f = point_calculate(t, x)
-    global mu
+    mu = 3.986 * 1e14;
     r = sqrt(x(1)^2 + x(2)^2 + x(3)^2);
     f = [x(4); x(5); x(6); (-mu/r^3)*x(1); (-mu/r^3)*x(2); (-mu/r^3)*x(3)];
     
--- point_dyn.m
+++ point_dyn.m
@@ -1,10 +1,10 @@
 function point_dyn()
-global mu
-
+global  Rer gm
 mu = 3.986 * 1e14;
+gm = 5972e24;
 alt = 600000;
 Rer = 6371000;
-
+pi2 = 2*pi;
 mean_motion = sqrt(mu/(Rer+alt)^3);
 
 Torb = 2 *pi / mean_motion;
@@ -13,7 +13,18 @@ x0 = Rer + alt; y0 = 0; z0 = 0;
 vx0 = 0; vy0 = x0*mean_motion; vz0 = 0;
 
 [tp, hp] = ode45(@point_calculate, [0 Torb], [x0; y0; z0; vx0; vy0; vz0]);
-xp = hp(:,1); yp = hp(:,2); zp = hp(:,3);
+xp = hp(:,1); yp = hp(:,2); zp = hp(:,3); vxp = hp(:,4); vyp = hp(:,5); vzp = hp(:,6);
+
+res = zeros(101, 2);
+
+for i = 1:1:size(xp)
+   
+    test = pkepler([xp(i); yp(i); zp(i)], [vxp(i); vyp(i); vzp(i)],1,1,1);
+    res(i, 1:2) = test;
+end
+
+axis equal;
 
 plot3(xp, yp, zp, 'r-');
+
 end
